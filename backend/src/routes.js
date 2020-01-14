30ff640728f230e375e69037e47b6d43a0d1a6da
Filipe de Leonel Batista <filipe.batista@salux.local>
2020-01-14 16:18:02 -0300

    Momento 01:18:27

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,27 +1,9 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController')
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) =>{
-    const { github_username, techs } = request.body
-
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-
-    const { name = login, avatar_url, bio } = apiResponse.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-    console.log(techsArray);
-    const dev = await Dev.create({
-        name,
-        github_username,
-        bio,
-        avatar_url,
-        techs: techsArray,
-    });
-
-    return response.json(dev);
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
