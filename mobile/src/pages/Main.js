440eabb669132d22ed97c9064ba98dd065387659
Filipe de Leonel Batista <filipe.batista@salux.local>
2020-01-16 18:06:44 -0300

    ajustes mapa

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,58 @@
-import React from 'react';
-import { View } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
-    return <MapView style={{flex: 1}} />
+    const [ currentRegion, setCurrentRegion ] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition(){
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted){
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                });
+            }
+        }
+        
+        loadInitialPosition();
+    }, []);
+    if(!currentRegion){
+        return null;
+    }
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map} >
+            <Marker coordinate={{latitude: -30.056931, longitude: -51.1899153 }}>
+                <Image style={styles.avatar} source={{ uri: "https://avatars1.githubusercontent.com/u/20867392?v=4" }} />
+            </Marker>
+        </MapView>
+    )
 }
+
+const styles = StyleSheet.create({
+    map:{
+        flex: 1
+    },
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff',
+
+    },
+
+});
 
 export default Main;
